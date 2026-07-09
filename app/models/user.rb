9e1c5fb3deb1d3d6ad5cc021d8bc91dd30955c81
Le Tran Khanh An <anltk@zigexn-v163s-MacBook-Pro.local>
2026-07-10 00:34:07 +0700

    Fix magic numbers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase } # or you can use "email.downcase!" as alternative
-  validates :name, presence: true, length: { maximum: 50 }
+  NAME_MAX_LENGTH = 50
+  EMAIL_MAX_LENGTH = 255
+  PASSWORD_MIN_LENGTH = 6
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  before_save { self.email = email.downcase } # or you can use "email.downcase!" as alternative
+  validates :name, presence: true, length: { maximum: NAME_MAX_LENGTH }
+  validates :email, presence: true, length: { maximum: EMAIL_MAX_LENGTH }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: PASSWORD_MIN_LENGTH }
 end
