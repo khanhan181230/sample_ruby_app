881a046a2a325bb340a7eb2fa3d59b3849d2444b
Le Tran Khanh An <anltk@zigexn-v163s-MacBook-Pro.local>
2026-07-13 16:27:37 +0700

    Finish chapter 7 signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)           # Not the final implementation, just a placeholder for now
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
