20fe27fe37c68069407c4fbb4df31c2fe882749c
Le Tran Khanh An <anltk@zigexn-v163s-MacBook-Pro.local>
2026-07-13 17:03:32 +0700

    Added minor lint fixes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
-  # Define magic number
-  DEFINE_GRAVATAR_SIZE=80
+  include Digest
 
-  #Return the Gravatar for the given user.
-  def gravatar_for(user, options = {size: DEFINE_GRAVATAR_SIZE})
+  DEFINE_GRAVATAR_SIZE = 80
+
+  def gravatar_for(user, options = { size: DEFINE_GRAVATAR_SIZE })
     size = options[:size]
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
